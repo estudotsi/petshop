3ecad310eb9b0b30731f686f45da200aa500b38d
rogerio.campos <rogerio.campos@cg.df.gov.br>
2023-03-06 10:06:52 -0300

    06/03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './template/account/login-page/login-page.component';
+import { PetsPageComponent } from './template/account/pets-page/pets-page.component';
+import { ResetPasswordPageComponent } from './template/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './template/account/signup-page/signup-page.component';
+import { FramePageComponent } from './template/master/frame.page';
+import { CartPageComponent } from './template/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './template/store/products-page/products-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path: '', component:  FramePageComponent,
+    children: [
+      { path: '', component: ProductsPageComponent },
+      { path: 'cart', component: CartPageComponent }
+    ]
+  },
+  {
+    path: 'account', component: FramePageComponent,
+    children: [
+      { path: 'pets', component: PetsPageComponent }
+    ]
+  },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'reset-password', component: ResetPasswordPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginPageComponent } from './template/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './template/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './template/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './template/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './template/store/products-page/products-page.component';
+import { CartPageComponent } from './template/store/cart-page/cart-page.component';
+import { FramePageComponent } from './template/master/frame.page';
+import { ProductCardComponent } from './componentes/product-card/product-card.component'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent,
+    FramePageComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
